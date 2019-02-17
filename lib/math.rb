54ea43f524bdfbc6aaf2c0634506a0e874f09df2
Okunola Jelilat Dasola <jelilatokunola2809@gmail.com>
2019-02-17 13:56:41 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,21 +1,31 @@
 def addition(num1, num2)
+  num1 + num2
 end
 
 def subtraction(num1, num2)
+  num1 - num2
 end
 
 def division(num1, num2)
+  num2 = 50
+  num1 = 2
+  num2 / num1
 end
 
 def multiplication(num1, num2)
+  num1 * num2
 end
 
 def modulo(num1, num2)
+  num2 = 34
+  num1 = 5
+  num2%num1
 end
 
 def square_root(num)
+ Math.sqrt(num)
 end
 
 def order_of_operation(num1, num2, num3, num4)
-  #Hint:  __ + (( __ * __ ) / __ )
+num1 + ((num2 * num3) /num4)  #Hint:  __ + (( __ * __ ) / __ )
 end
